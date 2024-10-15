160d0a3ff3776b8728925cf8c0dd6c8786674f8e
Randi <RandiBudiPrananda@proton.me>
2024-10-15 12:12:03 +0700

    style(app): update header and footer styles with gradient and dark background

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="bg-blue-500 text-white text-center p-4">
+      <header className="bg-gradient-to-br from-[#0470a5] to-[#24b24c] text-white text-center text-lg font-semibold p-4">
         <h1>Randi Budi Prananda</h1>
       </header>
       <main className="flex-grow">
         <Home />
       </main>
-      <footer className="bg-gray-700 text-white text-center p-2">
+      <footer className="bg-[#222222] text-white text-center p-2">
         <p>Tuesday, 15 October 2024</p>
       </footer>
     </div>

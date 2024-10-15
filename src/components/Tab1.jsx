788824af7b1231d65389db145cd5e1330eb4c747
Randi <RandiBudiPrananda@proton.me>
2024-10-15 12:18:15 +0700

    feat(components): add input validation and submission in tab1 component

--- src/components/Tab1.jsx
+++ src/components/Tab1.jsx
@@ -1,8 +1,46 @@
-const Tab1 = () => {
+import { useState } from "react";
+import Button from "./Button";
+
+const Tab1 = ({ disabled, onSubmit }) => {
+  const [number, setNumber] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let parsedNumber = parseInt(number);
+    if (number === "" || parsedNumber < 1 || parsedNumber > 100000) {
+      setError("Please enter a number between 1 and 100000.");
+      setNumber("");
+      return;
+    }
+    setError("");
+    onSubmit(parsedNumber);
+  };
+
+  if (disabled) {
+    return null;
+  }
+
   return (
-    <div>
-      <p>Tab 1</p>
-    </div>
+    <>
+      <form className="tab-section" onSubmit={handleSubmit}>
+        <label htmlFor="number" className="text-lg font-bold mb-4">
+          Enter a number (1 - 100000)
+        </label>
+        <input
+          id="number"
+          type="number"
+          value={number}
+          onChange={handleChange}
+        />
+        {error && <p className="text-red-500">{error}</p>}
+        <input type="submit" value="Submit" />
+      </form>
+    </>
   );
 };
 

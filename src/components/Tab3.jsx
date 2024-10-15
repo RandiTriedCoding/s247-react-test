c54fadc873c88f4e34c4008dd00c92b4009e76bf
Randi <RandiBudiPrananda@proton.me>
2024-10-15 12:19:54 +0700

    feat(components): add missing number finder in tab3 component

--- src/components/Tab3.jsx
+++ src/components/Tab3.jsx
@@ -1,7 +1,30 @@
-const Tab3 = () => {
+import { useState } from "react";
+import Button from "./Button";
+import useFindMissingNumber from "../hooks/useFindMissingNumber";
+
+const Tab3 = ({ disabled, generatedNumbers }) => {
+  const [answer, setAnswer] = useState(0);
+
+  const handleClick = () => {
+    const largestMissingPositive = useFindMissingNumber(generatedNumbers);
+    setAnswer(largestMissingPositive);
+  };
+
+  if (disabled) {
+    return null;
+  }
+
   return (
-    <div>
-      <p>Tab 3</p>
+    <div className="tab-section">
+      <Button onClick={handleClick} className="mb-10">
+        Find Missing Number
+      </Button>
+      {answer !== 0 && (
+        <h1>
+          The largest missing positive number is{" "}
+          <span className="text-red-500">{answer}</span>
+        </h1>
+      )}
     </div>
   );
 };

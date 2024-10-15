66acfb88e64d90784fe02fb0713ba793faecc4d6
Randi <RandiBudiPrananda@proton.me>
2024-10-15 13:43:25 +0700

    fix(useGenerateNumbers): correct number generation range

--- src/hooks/useGenerateNumbers.js
+++ src/hooks/useGenerateNumbers.js
@@ -1,7 +1,7 @@
 const useGenerateNumbers = (inputNumber) => {
   const newNumbers = Array.from(
     { length: inputNumber },
-    () => Math.floor(Math.random() * 2000001) - 1000000,
+    () => Math.floor(Math.random() * (2000000 + 1)) - 1000000,
   );
   return newNumbers;
 };

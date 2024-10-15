a64e03aab73a16af123abaeb49d91dd584af286c
Randi <RandiBudiPrananda@proton.me>
2024-10-15 13:13:17 +0700

    style(app): enhance layout styling with background and shadow effects

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-gradient-to-br from-[#0470a5] to-[#24b24c] text-white text-center text-lg font-semibold p-4">
-        <h1>Randi Budi Prananda</h1>
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      <header className="bg-gradient-to-br from-[#0470a5] to-[#24b24c] text-white text-center text-lg font-semibold p-4 shadow-lg">
+        <h1 className="hover:underline transition duration-300">
+          Randi Budi Prananda
+        </h1>
       </header>
-      <main className="flex-grow">
+      <main className="flex-grow p-4">
         <Home />
       </main>
-      <footer className="bg-[#222222] text-white text-center p-2">
+      <footer className="bg-[#222222] text-white text-center py-3 mt-4">
         <p>Tuesday, 15 October 2024</p>
       </footer>
     </div>
